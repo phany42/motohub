2617f3082bd361ecc9ff76ca307379815f01f60b
Phaneendra Chowdary <your_github_email@example.com>
2026-02-20 17:52:20 +0530

    Remove old root frontend files after move to frontend directory

--- src/data/filters.js
+++
@@ -1,5 +0,0 @@
-export const filterOptions = {
-  years: [2025, 2024, 2023, 2022, 2021],
-  brands: ["Royal Enfield", "Honda", "KTM", "Yamaha", "TVS"],
-  priceRanges: ["Under 1L", "1L - 2L", "2L+"],
-};
